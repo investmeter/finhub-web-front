d51ddcc8a97c0c6b227fc8c1c554bb20aa8c436f
Ivan Averin <i.averin@gmail.com>
2021-02-14 19:41:13 +0300

    pass the value from typeHead and validate it

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -11,24 +11,39 @@ import {Typeahead, withAsync} from 'react-bootstrap-typeahead';
 
 import DatePicker from "react-datepicker";
 
+import {useForm, Controller} from "react-hook-form"
+import {yupResolver} from '@hookform/resolvers/yup'
+
+import * as yup from 'yup'
+
 import {ApolloClient, InMemoryCache, gql} from '@apollo/client';
 
-import { initializeApollo } from '../lib/apolloClient'
+import {initializeApollo} from '../lib/apolloClient'
 import * as _ from 'lodash'
+import React from "react";
+
+const schema = yup.object({
+    securityId: yup.number().positive().required(),
+    dateAdded: yup.date().required(),
+    price: yup.number().required(),
+    amount: yup.number().positive(),
+    totalPaid: yup.number(),
+    brokerFee: yup.number()
+});
+
 
 let AsyncTypeHead = withAsync(Typeahead)
 
-const SecuritiesSearchTypeHead= (props) => {
+const SecuritiesSearchTypeHead = (props) => {
     const [isLoading, setIsLoading] = useState(false);
     const [options, setOptions] = useState(props.options);
-
 
     // if (props.options !== undefined) {
     //     setOptions(props.options)
     // }
 
 
-    const handleSearch =(query) => {
+    const handleSearch = (query) => {
         setIsLoading(true);
 
         const client = initializeApollo()
@@ -36,33 +51,44 @@ const SecuritiesSearchTypeHead= (props) => {
         client.query({
             query: gql`query securities($query:String){
                         securities (search: $query) {
+                        value:id
                         company:title
                         ticker
                       }
         }`,
             variables: {
-                query:query
+                query: query
             },
-            context:{}
+            context: {}
         }).then(
             (res) => {
                 setOptions(res.data.securities)
                 setIsLoading(false)
             }
         )
-        }
+    }
 
-        return (
-            <AsyncTypeHead {...props} options={options} onSearch={handleSearch} isLoading={isLoading}>
-            </AsyncTypeHead>
-        )
+    return (
+        <AsyncTypeHead {...props} options={options} onSearch={handleSearch} isLoading={isLoading}
+                       onChange={(selected) => {
+                           console.log('Selected', selected);
+                           if (selected.length > 0) {
+                               props.setValue('securityId', selected[0].value, { shouldDirty: true, shouldValidate:true })
+                           } else {
+                               props.setValue('securityId', 0, { shouldDirty: true, shouldValidate:true })
+                           }
+                       }}
 
+        >
+        </AsyncTypeHead>
+    )
 }
 
 const RBTDatePicker = (props) => {
     const [startDate, setStartDate] = useState(new Date());
     return (
-        <DatePicker {...props} selected={startDate} onChange={date => setStartDate(date)}  className="rbt-input rbt-input-main form-control"/>
+        <DatePicker {...props} selected={startDate} onChange={date => setStartDate(date)}
+                    className="rbt-input rbt-input-main form-control"/>
     )
 }
 
@@ -83,14 +109,28 @@ function PortfolioAdd({assets}) {
         )
     }
 
+    // validation setup
+    const { register, control, handleSubmit, watch, errors, formState, setValue} = useForm(
+        {
+        mode: "onChange",
+        resolver: yupResolver(schema)
+    }
+    );
+
+    const onSubmit = (data) => {
+        console.log("Submitted", data)
+    }
+
+
     return (
-        <Layout isProtected={true} isSession={!!_.get(session,'user.apiToken')} userEmail={!!session && session.user.email} >
+        <Layout isProtected={true} isSession={!!_.get(session, 'user.apiToken')}
+                userEmail={!!session && session.user.email}>
             <Container>
                 <Row>&nbsp;</Row>
 
                 <h1>Portfolio</h1>
                 <h2>Add Item</h2>
-                <Form>
+                <Form onSubmit={handleSubmit(onSubmit)}>
                     <Form.Group controlId="formAssetType">
                         <Form.Label>Type of Instrument</Form.Label>
                         <Form.Control as='select'>
@@ -104,38 +144,40 @@ function PortfolioAdd({assets}) {
 
                     <Form.Group controlId="formAssetId">
                         <Form.Label>Instrument</Form.Label>
-
-                        <SecuritiesSearchTypeHead
-                            id='asset-id'
-                            highlightOnlyResult={true}
-                            onChange={(selected) => {
-                                // Handle selections...
-                            }}
-                            options={assets}
-                            labelKey={option => `${option.ticker} ${option.company}`}
-                            placeholder="Type ticker or company name"
-                            clearButton
-                            defaultOpen={true}
-
+                        <Controller as={SecuritiesSearchTypeHead}
+                                    id='asset-id'
+                                    name='securityId'
+                                    control={control}
+                                    highlightOnlyResult={true}
+                                    options={assets}
+                                    labelKey={option => `${option.ticker} ${option.company}`}
+                                    placeholder="Type ticker or company name"
+                                    clearButton
+                                    defaultOpen={true}
+                                    defaultValue={0}
+                                    setValue={setValue}
+                                    isValid={formState.dirtyFields.securityId && !errors.securityId}
+                                    isInvalid={errors.securityId}
                         >
-                        </SecuritiesSearchTypeHead>
+
+                        </Controller>
 
                     </Form.Group>
 
                     <Form.Group controlId="dateAdded">
                         <Form.Label>Date of deal</Form.Label>
                         <Form.Row>
-                           <Col><RBTDatePicker /></Col>
+                            <Col><RBTDatePicker/></Col>
                         </Form.Row>
                     </Form.Group>
 
                     <Form.Group>
                         <Form.Label>Price</Form.Label>
                         <Form.Row>
-                        <Col>
+                            <Col>
                                 <Form.Control placeholder="Average price of acquired securities"/>
-                        </Col>
-                            <Form.Label column >USD</Form.Label>
+                            </Col>
+                            <Form.Label column>USD</Form.Label>
                         </Form.Row>
                     </Form.Group>
 
@@ -147,7 +189,7 @@ function PortfolioAdd({assets}) {
                             <Col>
                                 <Form.Control placeholder='Number of securities'></Form.Control>
                             </Col>
-                            <Form.Label column >psc.</Form.Label>
+                            <Form.Label column>psc.</Form.Label>
 
                         </Form.Row>
                     </Form.Group>
@@ -158,7 +200,7 @@ function PortfolioAdd({assets}) {
                             <Col>
                                 <Form.Control placeholder="Total paid for securities"/>
                             </Col>
-                            <Form.Label column >USD</Form.Label>
+                            <Form.Label column>USD</Form.Label>
                         </Form.Row>
                     </Form.Group>
 
@@ -168,11 +210,11 @@ function PortfolioAdd({assets}) {
                             <Col>
                                 <Form.Control placeholder="fee"/>
                             </Col>
-                            <Form.Label column >USD</Form.Label>
+                            <Form.Label column>USD</Form.Label>
                         </Form.Row>
                     </Form.Group>
 
-                    <Button type="submit">Add to Portfolio</Button>
+                    <Button variant="primary" type="submit">Add to Portfolio</Button>
 
                 </Form>
 
@@ -182,7 +224,7 @@ function PortfolioAdd({assets}) {
     )
 }
 
-export const  getServerSideProps = async (ctx) => {
+export const getServerSideProps = async (ctx) => {
 
     const client = initializeApollo()
 
@@ -197,7 +239,7 @@ export const  getServerSideProps = async (ctx) => {
                         ticker
                       }
                 }`
-        });
+    });
 
 
     return {
@@ -206,7 +248,7 @@ export const  getServerSideProps = async (ctx) => {
                 assets: res.data.securities,
                 initialApolloState: client.cache.extract()
             }
-            //, revalidate: 1
+        //, revalidate: 1
     }
 
 }
